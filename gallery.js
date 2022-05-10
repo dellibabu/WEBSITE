6d7bc6edd7721b3a94b73914c1b83e78b2afa2f5
dellibabu <105187275+dellibabu@users.noreply.github.com>
2022-05-10 10:33:03 +0530

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Gallery.css';
+function Gallery(){
+    return(
+        <div className="galery">
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqYWcLDt5JgiouU4h422NJIFV1RNpIhp9qPg&usqp=CAU'  height='350px' width='450px' />
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO6MyaFAejVYoatUfRG9m3PufJp2-J4Atq5w&usqp=CAU'  height='350px' width='450px' />
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_KkiKOa5Sfydg72I6zx6ZpDU8CClFy6G7lg&usqp=CAU'   height='350px' width='450px'/>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROhiNyBneaox9ta-b5E2rf1BZn_cvl6PHClQ&usqp=CAU'  height='350px' width='450px'/>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROzGFgQ75O8rNkl1oJ9ezmQV1zn942U07w9g&usqp=CAU'  height='350px' width='450px'/>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB-4iifkEnVHyxe0Iapo8Pgub7xzmKl78rjw&usqp=CAU'  height='350px' width='450px'/>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREZ_wL9cp-XNKOszBoha9MKXU8XAoGLD6clg&usqp=CAU'  height='350px' width='450px'/>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdynXRnEwfWPQl_04SwgyAMZHOaW-zoyaj4g&usqp=CAU'   height='350px' width='450px'/>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnatqOIC0v-xk2BE62fGXb2ynnH0YzQfMWEw&usqp=CAU'   height='350px' width='450px'/>
+        </div>
+    )
+}
+export default Gallery;
