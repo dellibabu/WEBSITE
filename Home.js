36da507bf560f795c36bf245060011890f98a806
dellibabu <105187275+dellibabu@users.noreply.github.com>
2022-05-10 10:24:01 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './Home.css';
+function Home(){
+    return(
+        <div className="x">
+            <div className="y">
+            <h3>Programming</h3>
+            <h2>Coding,Scripting in high level 
+                and continues for styles.
+            </h2>
+            <p>A paragraph is a series of related sentences developing a central idea, called the topic. Try to think about paragraphs in terms of thematic unity: a paragraph is a sentence or a group of sentences that supports one central, unified idea. Paragraphs add one idea at a time to your broader argument</p>
+            <p>A paragraph is a series of related sentences developing a central idea, called the topic. Try to think about paragraphs in terms of thematic unity: a paragraph is a sentence or a group of sentences that supports one central, unified idea. Paragraphs add one idea at a time to your broader argument</p>
+            <button>Learn More</button>
+            </div>
+        <div className="t">
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkpHCGUepyja_eF2w1T-F40NtrU_XGTtznIw&usqp=CAU' alt='text'/>
+            </div>
+            
+        </div>
+    )
+}
+export default Home;
